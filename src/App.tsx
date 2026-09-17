def2b0d168876ec63f092870a6d4a71f8d4c24cd
mohan <lewsishamilton@gmail.com>
2026-09-17 14:14:35 +0530

    pdfs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Navbar from "./layouts/navbar"
 import Footer from "./layouts/footer"
 import HeroPage from "./pages/HomePage"
+import scintificPdf from "./assets/Scientific-committee.pdf"
+import PdfViewerSection from "./layouts/PdfViewer"
 import { Route,Routes } from "react-router-dom"
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<HeroPage/>}/>
+        <Route path="/committess/scientific" element={<PdfViewerSection title="Scientific Committee" subtitle="just a test to tesss subtitle" pdfUrl={scintificPdf}/>}/>
       </Routes>
       <Footer/>
    </>
